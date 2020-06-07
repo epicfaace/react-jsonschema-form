58f95f2d1b2412db759bc0ff615f9701abcac978
Siva Ramaswami <sramaswami11@gmail.com>
2020-06-07 15:25:52 -0400

    cleanup

--- packages/fluent-ui/src/SelectWidget/SelectWidget.tsx
+++ packages/fluent-ui/src/SelectWidget/SelectWidget.tsx
@@ -1,40 +1,8 @@
 import React from "react";
 import { Label, Dropdown } from "@fluentui/react";
 import { WidgetProps } from "@rjsf/core";
-import { utils } from "@rjsf/core";
-
-const { asNumber, guessType } = utils;
 
 const nums = new Set(["number", "integer"]);
-
-/**
- * This is a silly limitation in the DOM where option change event values are
- * always retrieved as strings.
- */
-const processValue = (schema: any, value: any) => {
-  // "enum" is a reserved word, so only "type" and "items" can be destructured
-  const { type, items } = schema;
-  if (value === "") {
-    return undefined;
-  } else if (type === "array" && items && nums.has(items.type)) {
-    return value.map(asNumber);
-  } else if (type === "boolean") {
-    return value === "true";
-  } else if (type === "number") {
-    return asNumber(value);
-  }
-
-  // If type is undefined, but an enum is present, try and infer the type from
-  // the enum values
-  if (schema.enum) {
-    if (schema.enum.every((x: any) => guessType(x) === "number")) {
-      return asNumber(value);
-    } else if (schema.enum.every((x: any) => guessType(x) === "boolean")) {
-      return value === "true";
-    }
-  }
-  return value;
-};
 
 const SelectWidget = ({
   schema,
@@ -55,16 +23,11 @@ const SelectWidget = ({
 
   const _onChange = ({}, value: any) => onChange(value.key);
 
-  const _onBlur = ({ target: { value } }: React.FocusEvent<HTMLInputElement>) =>
-    onBlur(id, processValue(schema, value));
-  const _onFocus = ({
-    target: { value },
-  }: React.FocusEvent<HTMLInputElement>) =>
-    onFocus(id, processValue(schema, value));
+  const _onBlur = (e: any) => onBlur(id, e.target.value);
 
-  const dropDown = (enumOptions as any[]) || [];
+  const _onFocus = (e: any) => onFocus(id, e.target.value);
 
-  const newOptions = dropDown.map(option => ({
+  const newOptions = (enumOptions as any[]).map(option => ({
     key: option.value,
     text: option.label,
   }));
@@ -80,7 +43,8 @@ const SelectWidget = ({
         disabled={disabled || readonly}
         onChange={_onChange}
         onBlur={_onBlur}
-        onFocus={_onFocus}></Dropdown>
+        onFocus={_onFocus}
+      />
     </>
   );
 };
