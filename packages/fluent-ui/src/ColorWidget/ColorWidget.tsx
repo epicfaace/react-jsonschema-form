1372538c4757fd959d222a34f4d7df8ac62f1282
Siva Ramaswami <sramaswami11@gmail.com>
2020-06-10 16:42:57 -0400

    Ported ColorWidget to Fluent UI theme

---
+++ packages/fluent-ui/src/ColorWidget/ColorWidget.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+import {
+  ColorPicker,
+  IColorPickerProps,
+  IColor,
+  IColorPickerStyles,
+  getColorFromString,
+  Label,
+} from "@fluentui/react";
+import { WidgetProps } from "@rjsf/core";
+import _pick from "lodash/pick";
+import { useConstCallback } from "@uifabric/react-hooks";
+
+const colorPickerStyles: Partial<IColorPickerStyles> = {
+  panel: { padding: 12 },
+  root: {
+    maxWidth: 352,
+    minWidth: 352,
+  },
+  colorRectangle: { height: 268 },
+};
+
+const styles_red = {
+  // TODO: get this color from theme.
+  color: "rgb(164, 38, 44)",
+  fontSize: 12,
+  fontWeight: "normal" as any,
+  fontFamily: `"Segoe UI", "Segoe UI Web (West European)", "Segoe UI", -apple-system, BlinkMacSystemFont, Roboto, "Helvetica Neue", sans-serif;`,
+};
+
+// Keys of IColorPickerProps from @fluentui/react
+const allowedProps = [
+  "componentRef",
+  "color",
+  "strings",
+  "alphaType",
+  "alphaSliderHidden",
+  "hexLabel",
+  "redLabel",
+  "greenLabel",
+  "blueLabel",
+  "alphaLabel",
+  "className",
+  "theme",
+  "styles",
+];
+
+const ColorWidget = ({
+  id,
+  schema,
+  options,
+  value,
+  required,
+  disabled,
+  readonly,
+  label,
+  onChange,
+  onBlur,
+  onFocus,
+}: WidgetProps) => {
+  const { enumOptions, enumDisabled } = options;
+
+  const white = getColorFromString("#ffffff")!;
+  const [color, setColor] = React.useState(white);
+  const [alphaType, setAlphaType] = React.useState<
+    IColorPickerProps["alphaType"]
+  >("alpha");
+  const [showPreview] = React.useState(true);
+
+  const updateColor = useConstCallback((ev: any, colorObj: IColor) => {
+    setColor(colorObj);
+    onChange(colorObj.hex);
+  });
+
+  const uiProps = _pick(options.props || {}, allowedProps);
+
+  return (
+    <>
+      <Label>
+        {label || schema.title}
+        {required && <span style={styles_red}>&nbsp;*</span>}
+      </Label>
+      <ColorPicker
+        color={color}
+        onChange={updateColor}
+        alphaType={alphaType}
+        showPreview={showPreview}
+        styles={colorPickerStyles}
+        {...uiProps}
+      />
+    </>
+  );
+};
+export default ColorWidget;
